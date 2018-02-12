f9c71a3e1ae2328df369afc7f3a7731fbbda3ebf
Satoshi Nagayasu <snaga@uptime.jp>
2018-02-12 13:12:12 +0900

    4. ページを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PipeComponent } from './pipe/pipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pipe',
+    component: PipeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
